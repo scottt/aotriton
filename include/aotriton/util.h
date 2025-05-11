9f664637388d49e39fe15696c5d2101d295805d0
Scott Tsai <scottt.tw@gmail.com>
2025-05-12 05:10:21 +0800

    aotriton/util.h: comment out extern template class TensorView to fix linking on Windows

--- include/aotriton/util.h
+++ include/aotriton/util.h
@@ -176,12 +176,10 @@ private:
   DType dtype_ = kUnknown;
 };
 
-#ifndef aotriton_v2_EXPORTS
-extern template class TensorView<1>;
-extern template class TensorView<2>;
-extern template class TensorView<3>;
-extern template class TensorView<4>;
-#endif // aotriton_v2_EXPORTS
+// extern template class TensorView<1>;
+// extern template class TensorView<2>;
+// extern template class TensorView<3>;
+// extern template class TensorView<4>;
 
 Gpu AOTRITON_API getGpuFromStream(hipStream_t);
 bool AOTRITON_API isArchExperimentallySupported(hipStream_t);
